ca2e66177f4f906053815459fd7e2d823f1741c7
Jonathan Rivers <jrrivers@gmail.com>
2017-03-19 22:31:02 +0000

    Modify post controller, add post form and show views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+    
   def index
     @post = Post.all.order("created_at DESC")
   end
@@ -15,6 +17,36 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+  
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  
+  def find_post
+    @post = Post.find(params[:id])
   end
   
 end
